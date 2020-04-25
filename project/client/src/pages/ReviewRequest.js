cc47f396dac0a9b68e7564eca08303f375c4d6be
Peter Walker <ptwalker@umass.edu>
2020-04-25 11:28:35 -0400

    linked submit-view
    I added a link to write evalutations in REviewRequest

--- project/client/src/pages/ReviewRequest.js
+++ project/client/src/pages/ReviewRequest.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-class ReviewRequest extends Component{
-    render(){
-        return(
-            <h1>
-                Review Request
-            </h1>
-        )
-    }
+class ReviewRequest extends Component {
+  render() {
+    return (
+      <h1>
+        Review Request
+        <h2>
+          <a style={{ align: "center" }} href="submit-view">
+            Write evaluation
+          </a>
+        </h2>
+      </h1>
+    );
+  }
 }
 
 export default ReviewRequest;
