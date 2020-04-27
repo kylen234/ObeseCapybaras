e524b2e04fa25054de1e59ab26efef63363c5a96
aylinelmali <60710147+aylinelmali@users.noreply.github.com>
2020-04-27 15:53:01 -0400

    change direct reports to manager view

--- project/client/src/pages/index.js
+++ project/client/src/pages/index.js
@@ -2,7 +2,7 @@ import RequestEvaluations from './RequestEvaluations'
 import Submit from './Submit'
 import ViewEvaluations from './ViewEvaluations'
 import MyProfile from './MyProfile'
-import DirectReports from './DirectReports'
+import ManagerView from './ManagerView'
 import ReviewRequest from './ReviewRequest'
 
-export {Submit, RequestEvaluations, ViewEvaluations, MyProfile, DirectReports, ReviewRequest}
+export {Submit, RequestEvaluations, ViewEvaluations, MyProfile, ManagerView, ReviewRequest}
