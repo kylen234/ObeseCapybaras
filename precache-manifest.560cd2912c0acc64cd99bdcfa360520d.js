6b39f1ee06ffe63e17dbbfbe0f8c71f684ae55bb
zhengcharlie8 <44006237+zhengcharlie8@users.noreply.github.com>
2020-05-03 14:27:50 -0400

    Updates

---
+++ precache-manifest.560cd2912c0acc64cd99bdcfa360520d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31cea16c066bbb18cc63862598aa2fbd",
+    "url": "/ObeseCapybaras/index.html"
+  },
+  {
+    "revision": "dbcc071e1eed5205094b",
+    "url": "/ObeseCapybaras/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "136ceccf6f6dd4267ca8",
+    "url": "/ObeseCapybaras/static/css/main.299ac0d8.chunk.css"
+  },
+  {
+    "revision": "dbcc071e1eed5205094b",
+    "url": "/ObeseCapybaras/static/js/2.7b2a1f27.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ObeseCapybaras/static/js/2.7b2a1f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136ceccf6f6dd4267ca8",
+    "url": "/ObeseCapybaras/static/js/main.736bf375.chunk.js"
+  },
+  {
+    "revision": "516cc467965e104d7916",
+    "url": "/ObeseCapybaras/static/js/runtime-main.0079f483.js"
+  },
+  {
+    "revision": "8135297abf62fcc35f1cfc498ad3cc27",
+    "url": "/ObeseCapybaras/static/media/logo.8135297a.png"
+  }
+]);
