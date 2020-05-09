e67fdbcc246efc519bc577adefdf03d1a90a50ad
Timothy Nguyen <quynhthoa1972@gmail.com>
2020-05-09 19:56:15 -0400

    Let's see if this fixes the server

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -56,7 +56,7 @@ class Login extends Component {
 
         return (
             <div className="container">
-                <Link to='/'/>
+                <!--<Link to='/'/>-->
                 {(checkCookie() !== null) ? <Redirect to='/MyProfile' /> : <Redirect to='/'/>}
                 {(!this.props.isSuccess) ? <div className='error'>{this.props.messages}</div> : null}
                 <div style={{marginTop: "4rem"}} className="row">
